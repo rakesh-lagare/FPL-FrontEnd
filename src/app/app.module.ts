780fe31771b98903a0382cddef64079d40101996
Rakesh Lagare <rakesh.lagare@gmail.com>
2020-12-01 16:41:20 +0100

    Initial push.. Devya work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ButtonModule, CardModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
